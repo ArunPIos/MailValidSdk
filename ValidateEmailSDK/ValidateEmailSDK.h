aaf7e587ad739e5a98cdde9c9f61d442f4c6d501
ArunPIos <arunpandian2041@gmail.com>
2024-03-06 14:31:34 +0530

    First project upload

---
+++ ValidateEmailSDK/ValidateEmailSDK.h
@@ -0,0 +1,18 @@
+//
+//  ValidateEmailSDK.h
+//  ValidateEmailSDK
+//
+//  Created by Arun_Pandian on 06/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ValidateEmailSDK.
+FOUNDATION_EXPORT double ValidateEmailSDKVersionNumber;
+
+//! Project version string for ValidateEmailSDK.
+FOUNDATION_EXPORT const unsigned char ValidateEmailSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ValidateEmailSDK/PublicHeader.h>
+
+
